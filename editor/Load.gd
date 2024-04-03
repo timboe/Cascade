389734bc8a0d778c12974710b266825223fa139e
Tim Martin <timboe@gmail.com>
2024-04-03 18:31:12 +0100

    Update editor

---
+++ editor/Load.gd
@@ -0,0 +1,81 @@
+extends Button
+
+func _on_file_dialog_confirmed():
+	print("LOAD ", $FileDialog.current_file)
+	
+	var save_game_json := FileAccess.get_file_as_string($FileDialog.current_file)
+	var json := JSON.new()
+	var error = json.parse(save_game_json)
+	if error == OK:
+		var save_game : Dictionary = json.data
+		restore_save(save_game)
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", save_game_json, " at line ", json.get_error_line())
+	
+func _on_file_dialog_canceled():
+	print("SAVE Cancelled")
+
+func _on_pressed():
+	$FileDialog.visible = true
+
+func populate_static(save_game : Dictionary, static_instance : Control) -> void:
+	if save_game["shape"] == 0:
+		static_instance.find_child("CheckRect").button_pressed = 1
+	static_instance.find_child("XText").value = save_game["x"]
+	static_instance.find_child("YText").value = save_game["y"]
+	static_instance.find_child("AngleText").value = save_game["angle"]
+	static_instance.find_child("SizeText").selected = save_game["size"] 
+	static_instance.find_child("TypeText").selected = save_game["type"]
+	
+func populate_elliptic(save_game : Dictionary, elliptic_instance : Control) -> void:
+	if save_game["shape"] == 0:
+		elliptic_instance.find_child("CheckRect").button_pressed = 1
+	elliptic_instance.find_child("XText").value = save_game["x"]
+	elliptic_instance.find_child("YText").value = save_game["y"]
+	elliptic_instance.find_child("AngleText").value = save_game["angle"]
+	elliptic_instance.find_child("SizeText").selected = save_game["size"] 
+	elliptic_instance.find_child("ArcText").value = save_game["arc"]
+	elliptic_instance.find_child("ArcAngleCheckbox").button_pressed = save_game["use_arc"]
+	elliptic_instance.find_child("SpeedText").value = save_game["speed"]
+	elliptic_instance.find_child("AText").value = save_game["a"]
+	elliptic_instance.find_child("BText").value = save_game["b"]
+	elliptic_instance.find_child("PathSlider").value = save_game["n_pegs"]
+	# Changing PathSliderPathSlider. value will populate the sub-nodes already, just need to set them
+	for i in range(0, save_game["n_pegs"]):
+		var peg_name : String = "PegContainer"+str(i+1)
+		var peg_container : Control = elliptic_instance.find_child(peg_name, true, false)
+		var peg_save : Dictionary = save_game[peg_name]
+		peg_container.find_child("ShapeButton").selected = peg_save["shape"]
+		peg_container.find_child("SizeButton").selected = peg_save["size"]
+		peg_container.find_child("TypeButton").selected = peg_save["type"]
+
+func reset_level() -> void:
+	var i := 0
+	for peg in get_tree().get_nodes_in_group("pegs"):
+		peg.name = "temp" + str(i)
+		peg.queue_free()
+		i += 1
+
+func restore_save(save_game : Dictionary) -> void:
+	reset_level()
+	
+	$%WorldSlider.value = save_game["world"] 
+	$%LevelSlider.value = save_game["level"]
+	
+	var static_pegs : int = save_game["n_static"]
+	var elliptic_paths : int = save_game["n_elliptic"]
+	var linear_paths : int = save_game["n_linear"]
+	
+	$%AddStatic.reset_count()
+	for i in range(1, static_pegs+1):
+		$%AddStatic._on_pressed()
+		var static_name = "StaticControl" + str(i)
+		var static_instance = $%RightVBox.find_child(static_name, true, false)
+		populate_static(save_game[static_name], static_instance)
+		
+	$%AddElliptic.reset_count()
+	for i in range(1, elliptic_paths+1):
+		$%AddElliptic._on_pressed()
+		var elliptic_name = "EllipticControl" + str(i)
+		var elliptic_instance = $%RightVBox.find_child(elliptic_name, true, false)
+		populate_elliptic(save_game[elliptic_name], elliptic_instance)
--- editor/PathSlider.gd
+++ editor/PathSlider.gd
@@ -5,21 +5,17 @@ extends HSlider
 @onready var rend = $/root/RootControl.find_child("Render")
 @onready var parentControl : Control = get_parent().get_parent().get_parent().get_parent()
 
-var total_peg = 1
-var total_line = 1
-
-func get_y_size() -> int:
-	if "LineControl" in parentControl.name:
-		print("resize for pegs ", total_peg," line segments ", total_line)
-		return 340 + ((total_peg+1) * (27+6)) + ((total_line+1) * (27+6))
-	return 320 + ((total_peg+1) * (27+6))
-
 func _on_value_changed(newValue):
 	$"../PathLabel".text = "Path Pegs: " + str(newValue)
 	rend.do_update()
 	var pegVbox : VBoxContainer = parentControl.find_child("PegVBox")
-	total_peg = newValue
-	parentControl.custom_minimum_size.y = get_y_size()
+	var tot_pegs = newValue
+	var tot_lines_node = $"../../HBoxContainer3/LineSlider"
+	if tot_lines_node:
+		var tot_lines = tot_lines_node.value
+		parentControl.custom_minimum_size.y = 340 + ((tot_lines+1) * (27+6)) + ((tot_pegs+1) * (27+6))
+	else:
+		parentControl.custom_minimum_size.y = 320 + ((tot_pegs+1) * (27+6))
 	for i in range(1, newValue+1):
 		var pName = "PegContainer"+str(i)
 		var pegControl = pegVbox.find_child(pName,true,false)
@@ -41,13 +37,15 @@ func _on_line_slider_value_changed(newValue):
 	$"../LineLabel".text = "Path Segments:" + str(newValue)
 	rend.do_update()
 	var lineVbox : VBoxContainer = parentControl.find_child("LineVBox")
-	total_line = newValue
-	parentControl.custom_minimum_size.y = get_y_size()
+	# Also need number of pegs
+	var tot_pegs = $"../../HBoxContainer2/PathSlider".value
+	var tot_lines = newValue
+	parentControl.custom_minimum_size.y = 340 + ((tot_lines+1) * (27+6)) + ((tot_pegs+1) * (27+6))
 	for i in range(1, newValue+1):
 		var pName = "LineContainer"+str(i)
 		var lineControl = lineVbox.find_child(pName,true,false)
 		if lineControl == null:
-			print("add line to ",lineVbox)
+			#print("add line to ",lineVbox)
 			var instance = lineScene.instantiate()
 			instance.name = pName
 			lineVbox.add_child(instance)
