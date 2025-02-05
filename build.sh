b0f456324bf502b6d8b34b3c209f6f5fef77f73d
Tim Martin <timboe@gmail.com>
2025-02-05 23:51:07 +0100

    add updated holl images

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ mv tmp Source/pdxinfo
 
 cd ${PLAYDATE_SDK_PATH}/Disk/Data/uk.co.tim-martin.cascada/
 for f in *.bmp; do convert $f -resize 50% -dither FloydSteinberg -monochrome "`basename $f .bmp`.png"; done
+rm *bmp
 cd -
 rm Source/images/holes/*png
-mv ${PLAYDATE_SDK_PATH}/Disk/Data/uk.co.tim-martin.cascada/*png Source/images/holes
+cp ${PLAYDATE_SDK_PATH}/Disk/Data/uk.co.tim-martin.cascada/*png Source/images/holes
 rm ${PLAYDATE_SDK_PATH}/Disk/Data/uk.co.tim-martin.cascada/*bmp
 
+make clean
 make
 
 zip -r -q Cascada_v0.1.pdx.zip Cascada.pdx
